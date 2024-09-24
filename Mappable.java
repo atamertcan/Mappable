720b8a98fc6dafb8d74afd33810fd60cb10ec440
atamertcan <142722122+atamertcan@users.noreply.github.com>
2024-09-24 19:37:18 +0300

    Mappable.java

--- Mappable.java
+++ Mappable.java
@@ -7,3 +7,20 @@ public interface Mappable {
     String JSON_PROPERTY = """
             "properties":{%s}
             """;
+
+    default void toJSON(){
+        String printString = String.format("\"type\" : \"%s\", \"label\" : \"%s\", \"marker\" : \"%s\" ",
+                getType(), getLabel(), getMarker() );
+        System.out.print(printString);
+
+    }
+
+    static void printMapProperties(Mappable mappable){
+
+        String jsonString = String.format(JSON_PROPERTY, mappable.toString());
+        System.out.println("{" + jsonString + ", ");
+        mappable.toJSON();
+        System.out.println("}");
+
+    }
+}
