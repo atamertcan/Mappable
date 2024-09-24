ddb3108a2051abfdb82522bea453a6653165197d
atamertcan <142722122+atamertcan@users.noreply.github.com>
2024-09-24 19:34:45 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,16 @@
+public class Main {
 
+    public static void main(String[] args) {
+
+        Building building = new Building("Ankara Town Hall", UsageType.GOVERNMENT, GeometryType.POINT, Color.WHITE, Marker.CIRCLE);
+        Mappable.printMapProperties(building);
+
+        System.out.println("-".repeat(150));
+
+        UtilityLine ul = new UtilityLine("Istiklal st.",UtilityType.FIBER_OPTIC, GeometryType.LINE, Color.BLUE, Marker.DOTTED);
+        Mappable.printMapProperties(ul);
+
+
+    }
+
+}
